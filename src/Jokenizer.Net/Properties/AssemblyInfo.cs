3038ef22d716cb1889d2bc91f1bb2f6024a7f461
Umut Özel <umutozel@outlook.com>
2018-09-19 22:06:16 +0300

    add assembly info

---
+++ src/Jokenizer.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jokenizer.Net - C# Expression parser and evaluator")]
+[assembly: AssemblyDescription("Tokenize C# expressions and convert them to Lambda or Func later.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Umut Özel")]
+[assembly: AssemblyProduct("Jokenizer.Net")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
